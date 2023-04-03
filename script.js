bcf9f8b3d7e475cb73952696d1e9c82981332e18
farzad dianat <farzad.dianat@gmail.com>
2023-04-04 03:50:48 +0430

    Add form popup

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 /* eslint-disable no-alert */
 const myLibrary = [];
+const table = document.querySelector('table');
+const openModalButtons = document.querySelector('.new__book')
+const closeModalButtons = document.querySelector('.close__btn')
+const overlay = document.getElementById('overlay')
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -8,11 +12,53 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
-  const book = Object.create(Book)
-  book.title = prompt('Title of the Book?')
-  book.author = prompt('Author of the Book?')
-  book.pages = prompt('Number of the pages of the Book?')
-  book.read = prompt('You already read the Book. true or false?')
-  myLibrary.push(book)
+function addBookToTable() {
+  myLibrary.forEach((book) => {
+    const template = `
+    <tr>
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.pages}</td>
+      <td>${book.read}</td>
+      `;
+    table.innerHTML += template;
+    myLibrary.pop();
+  });
 }
+
+function addBookToLibrary() {
+  const book = Object.create(Book);
+  book.title = prompt('Title of the Book?');
+  book.author = prompt('Author of the Book?');
+  book.pages = prompt('Number of the pages of the Book?');
+  book.read = prompt('You already read the Book. true or false?');
+  myLibrary.push(book);
+  addBookToTable();
+}
+
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+openModalButtons.addEventListener('click', () => {
+  const modal = document.querySelector('.modal')
+  openModal(modal)
+})
+closeModalButtons.addEventListener('click', () => {
+  const modal = document.querySelector('.modal')
+  closeModal(modal)
+})
+
+overlay.addEventListener('click', () => {
+  const modal = document.querySelector('.modal.active')
+  closeModal(modal)
+})
