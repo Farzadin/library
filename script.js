62deb252478b925dc05b1dee78fbccf954101313
farzad dianat <farzad.dianat@gmail.com>
2023-04-05 01:49:40 +0430

    Fix: books no longer can have same id

--- script.js
+++ script.js
@@ -1,64 +1,79 @@
-/* eslint-disable no-alert */
 const myLibrary = [];
+const bookIds = [];
 const table = document.querySelector('table');
-const openModalButtons = document.querySelector('.new__book')
-const closeModalButtons = document.querySelector('.close__btn')
-const overlay = document.getElementById('overlay')
+const openModalButton = document.querySelector('.new__book');
+const closeModalButton = document.querySelector('.close__btn');
+const submitButton = document.querySelector('.submit');
+const overlay = document.getElementById('overlay');
 
-function Book(title, author, pages, read) {
+function Book(id, title, author, pages, read) {
+  this.id = id;
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
 
-function addBookToTable() {
-  myLibrary.forEach((book) => {
-    const template = `
-    <tr>
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td>${book.pages}</td>
-      <td>${book.read}</td>
-      `;
-    table.innerHTML += template;
-    myLibrary.pop();
+function displayBooks() {
+  myLibrary.forEach((item) => {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${item.id}</td>
+      <td>${item.title}</td>
+      <td>${item.author}</td>
+      <td>${item.pages}</td>
+      <td>${item.read}</td>`;
+
+    table.appendChild(row);
   });
 }
 
-function addBookToLibrary() {
-  const book = Object.create(Book);
-  book.title = prompt('Title of the Book?');
-  book.author = prompt('Author of the Book?');
-  book.pages = prompt('Number of the pages of the Book?');
-  book.read = prompt('You already read the Book. true or false?');
-  myLibrary.push(book);
-  addBookToTable();
+function addBook(event) {
+  event.preventDefault();
+
+  const id = document.querySelector('#id').value;
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const read = document.querySelector('#read');
+
+  if (read.checked === true) read.value = 'Already read it';
+  if (read.checked === false) read.value = 'Not read yet';
+
+  const newBook = new Book(id, title, author, pages, read.value);
+
+  if (!bookIds.includes(newBook.id)) {
+    bookIds.push(newBook.id);
+    myLibrary.push(newBook);
+    displayBooks();
+    myLibrary.pop();
+  }
 }
 
+submitButton.addEventListener('click', addBook);
 
 function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
 }
 
 function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
 }
 
-openModalButtons.addEventListener('click', () => {
-  const modal = document.querySelector('.modal')
-  openModal(modal)
-})
-closeModalButtons.addEventListener('click', () => {
-  const modal = document.querySelector('.modal')
-  closeModal(modal)
-})
+openModalButton.addEventListener('click', () => {
+  const modal = document.querySelector('.modal');
+  openModal(modal);
+});
+closeModalButton.addEventListener('click', () => {
+  const modal = document.querySelector('.modal');
+  closeModal(modal);
+});
 
 overlay.addEventListener('click', () => {
-  const modal = document.querySelector('.modal.active')
-  closeModal(modal)
-})
+  const modal = document.querySelector('.modal.active');
+  closeModal(modal);
+});
